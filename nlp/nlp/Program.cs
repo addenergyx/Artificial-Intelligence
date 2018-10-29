92b4e154907702b985489e3e61e8d199264d9d9c
David <david-adeniji@hotmail.co.uk>
2018-10-29 11:08:36 +0000

    week 6 npl

---
+++ nlp/nlp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nlp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = System.IO.File.ReadAllText("treasureisland.txt");
+            System.Console.WriteLine(input);
+            //System.Console.ReadLine();
+
+            System.Console.WriteLine("----------------------");
+            int cc = CharCount(input);
+            System.Console.WriteLine("----------------------");
+            int wc = WhiteSpaceCount(input);
+            System.Console.WriteLine("----------------------");
+            int vc = VowelCount(input);
+            System.Console.WriteLine("----------------------");
+            int coc = ConstantCount(input);
+            System.Console.WriteLine("----------------------");
+
+
+            System.Console.WriteLine(cc);
+            System.Console.WriteLine(wc);
+            System.Console.WriteLine(vc);
+            System.Console.WriteLine(coc);
+            System.Console.ReadLine();
+
+        }
+
+        private readonly char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+        static public int CharCount(string input)
+        {
+            return input.Split().Length;
+        }
+
+        static public int WhiteSpaceCount(string input)
+        {
+            return input.Count(Char.IsWhiteSpace);
+        }
+
+        static public int VowelCount(string input)
+        {
+            return input.ToLower().Count(x => input.Contains(x));
+        }
+
+        static public int ConstantCount(string input)
+        {
+            return input.ToLower().Count(x => input.Contains(x));
+        }
+    }
+}
